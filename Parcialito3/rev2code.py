ec0255be4840f33e22fc89b3e8ac215f6f1e211e
FdelMazo <FdelMazo@fi.uba.ar>
2022-07-12 23:14:14 -0300

    Modify rev2code to work w/ nx2 and py3

--- Parcialito3/rev2code.py
+++ Parcialito3/rev2code.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 # Code for running the REV2 algorithm from the paper "REV2: Fraudulent User Prediction in Rating Platforms"
-#
+# Modified to:
+#  work with networkx>=2.0
+#  use 1 as initial values for R F and G instead of birdnest
+#  pickle the graph result instead of using csvs
 
-import os
 import sys
 
 NETWORKNAME = sys.argv[1] #需要外部输入参数
@@ -20,75 +22,16 @@ gamma3 = int(sys.argv[8])
 if gamma1 == 0 and gamma2 == 0 and gamma3 == 0:
         sys.exit(0)
 
-import random
-import subprocess 
+import networkx as nx
+import _pickle as cPickle
 import numpy
-import time
-from collections import defaultdict
-import csv
-import networkx as nx  #python复杂网络分析库
-import datetime
-import math
-from math import exp
-import unicodecsv
-from chardet import detect  #detect(str),参数只能是str,不能是unicode编码的
-import cPickle
-#import detect_iat
 
 print ("Loading %s network" % NETWORKNAME)
-G = cPickle.load(open("./data/%s/%s_network.pkl" % (NETWORKNAME, NETWORKNAME), "rb"))
+G = cPickle.load(open("./data/%s_network.pkl" % (NETWORKNAME), "rb"))
 
-print ("Loaded")
-
-if NETWORKNAME in ["otc", "alpha"]:
-    num_ratings = 21
-else:
-    num_ratings = 5
-num_iat_bins = 49
-
-def get_timestamp_dist(node, normalize):#modified (added 2nd argument)
-    if "u" in node[0]:
-        edges = G.out_edges(node, data=True)
-        ts = [edge[2]["timestamp"] for edge in edges]
-        ts = numpy.array(sorted(ts))
-    else:
-        edges = G.in_edges(node, data=True)
-        ts = [edge[2]["timestamp"] for edge in edges]
-        ts = numpy.array(sorted(ts))
-
-    diff_ts = ts[1:] - ts[:-1]
-
-    y, x =  numpy.histogram(diff_ts, bins=numpy.logspace(0, 8, num_iat_bins+1))
-    y[0] = sum(numpy.array(diff_ts) == 0)
-    if sum(y)!=0.0 and normalize:
-        y = y*1.0/sum(y)
-    #for i in range(1, len(y)):# In[1]:
-
-    #y[i] += y[i-1]
-    return x,y
-
-
-def get_rating_dist(node, normalize):
-    if "u" in node[0]:
-        edges = G.out_edges(node, data=True)
-        ts = [int(round(10*edge[2]["weight"])) for edge in edges]
-    else:
-        edges = G.in_edges(node, data=True)
-        ts = [int(round(10*edge[2]["weight"])) for edge in edges]
-
-    if NETWORKNAME in ["otc", "alpha"]: 
-        y, x =  numpy.histogram(ts, bins=range(-10, 12))
-    else:
-        y, x =  numpy.histogram(ts, bins=range(0, 6))
-
-    if sum(y)!=0.0 and normalize:
-        y = y*1.0/sum(y)
-    return x,y
-
-
-nodes = G.nodes() #node represent user
-edges = G.edges(data=True) #edge represent rating
-print ("%s network has %d nodes and %d edges" % (NETWORKNAME, len(nodes), len(edges)))
+nodes = G.nodes()
+edges = G.edges(data=True)
+print("%s network has %d nodes and %d edges" %  (NETWORKNAME, len(nodes), len(edges)))
 
 user_names = [node for node in nodes if "u" in node]
 product_names = [node for node in nodes if "p" in node]
@@ -97,32 +40,13 @@ num_products = len(product_names)
 user_map = dict(zip(user_names, range(len(user_names))))
 product_map = dict(zip(product_names, range(len(product_names))))
 
-full_birdnest_user = cPickle.load(open("data/%s/%s_birdnest_user.pkl" % (NETWORKNAME, NETWORKNAME), "rb"))
-full_birdnest_product = cPickle.load(open("data/%s/%s_birdnest_product.pkl" %(NETWORKNAME, NETWORKNAME), "rb"))
-
-full_birdnest_edge = []
-try:
-    print ('loading birdnest pickle')
-    full_birdnest_edge = cPickle.load(open("./data/%s/%s_edge_birdnest.pkl" % (NETWORKNAME, NETWORKNAME),"rb"))
-    edge_map = cPickle.load(open("./data/%s/%s_edge_map.pkl" % (NETWORKNAME, NETWORKNAME),"rb"))
-    full_birdnest_edge = numpy.array(full_birdnest_edge)
-    mn = min(full_birdnest_edge)
-    mx = max(full_birdnest_edge)
-    full_birdnest_edge = (full_birdnest_edge - mn)*1.0/(mx-mn+0.001)
-except:
-    print ("Didnt find edge birdnest scores for %s network" % NETWORKNAME)
-    full_birdnest_edge = [0.0]*len(edges)
-    ae = zip(numpy.array(edges)[:,0], numpy.array(edges)[:, 1])
-    edge_map = dict(zip(ae, range(len(edges))))
-
-for node in nodes:
-    if "u" in node[0]:
-        G.node[node]["fairness"] = 1 - full_birdnest_user[user_map[node]]
-    else:
-        G.node[node]["goodness"] = (1 - full_birdnest_product[product_map[node]] - 0.5)*2
-
+for node in user_names:
+    G.nodes[node]["fairness"] = 1
+for node in product_names:
+    G.nodes[node]["goodness"] = 1
 for edge in edges:
-    G[edge[0]][edge[1]]["fairness"] = 1 - full_birdnest_edge[edge_map[(edge[0], edge[1])]]
+    G[edge[0]][edge[1]]["fairness"] = 1
+
 iter = 0
 
 yfg = []
@@ -152,7 +76,7 @@ while iter < 500:
     for node in nodes:
      if "p" not in node[0]:
         continue
-     currentgvals.append(G.node[node]["goodness"])
+     currentgvals.append(G.nodes[node]["goodness"])
     
     median_gvals = numpy.median(currentgvals) # Alternatively, we can use mean here, intead of median
                                               # 对应mu(g)公式
@@ -161,14 +85,14 @@ while iter < 500:
         if "p" not in node[0]:
             continue
         
-        inedges = G.in_edges(node,  data=True)
+        inedges = G.edges(node,  data=True)
         ftotal = 0.0 # 对应In(p)公式
         gtotal = 0.0
         for edge in inedges:
             gtotal += edge[2]["fairness"]*edge[2]["weight"]
         ftotal += 1.0
-        
-        kl_timestamp = ((1 - full_birdnest_product[product_map[node]]) - 0.5)*2 #对应P(p)公式
+
+        kl_timestamp = 1  #对应P(p)公式
 
         if ftotal > 0.0:
             mean_rating_fairness = (beta1*median_gvals + beta2* kl_timestamp + gtotal)/(beta1 + beta2 + ftotal) #对应公式G(p)
@@ -181,18 +105,18 @@ while iter < 500:
             x = -1.0
         if x > 1.0:
             x = 1.0
-        dp += abs(G.node[node]["goodness"] - x)
-        G.node[node]["goodness"] = x
+        dp += abs(G.nodes[node]["goodness"] - x)
+        G.nodes[node]["goodness"] = x
     
     ############################################################
     
     print ("Updating fairness of ratings")
     for edge in edges:
-        rating_distance = 1 - (abs(edge[2]["weight"] - G.node[edge[1]]["goodness"])/2.0) #对应公式R(u,p)分子中间一项
+        rating_distance = 1 - (abs(edge[2]["weight"] - G.nodes[edge[1]]["goodness"])/2.0) #对应公式R(u,p)分子中间一项
         
-        user_fairness = G.node[edge[0]]["fairness"] #对应F(u)
+        user_fairness = G.nodes[edge[0]]["fairness"] #对应F(u)
         ee = (edge[0], edge[1])
-        kl_text = 1.0 - full_birdnest_edge[edge_map[ee]] #对应BA R(u,p)
+        kl_text = 1.0  #对应BA R(u,p)
 
         x = (gamma2*rating_distance + gamma1*user_fairness + gamma3*kl_text)/(gamma1+gamma2 + gamma3) #对应公式R(u,p)
 
@@ -202,15 +126,15 @@ while iter < 500:
             x = 1.0
         
         dr += abs(edge[2]["fairness"] - x)
-        G.edge[edge[0]][edge[1]]["fairness"] = x
-    
+        G[edge[0]][edge[1]]["fairness"] = x
+
     ############################################################
     
     currentfvals = []
     for node in nodes:
      if "u" not in node[0]:
         continue
-     currentfvals.append(G.node[node]["fairness"])
+     currentfvals.append(G.nodes[node]["fairness"])
     median_fvals = numpy.median(currentfvals) # Alternatively, we can use mean here, intead of median
 
     print ('updating fairness of users')
@@ -218,7 +142,7 @@ while iter < 500:
         if "u" not in node[0]:
             continue
         
-        outedges = G.out_edges(node, data=True)
+        outedges = G.edges(node, data=True)
         
         f = 0
         rating_fairness = []
@@ -228,7 +152,7 @@ while iter < 500:
         for x in range(0,alpha1):
             rating_fairness.append(median_fvals)
 
-        kl_timestamp = 1.0 - full_birdnest_user[user_map[node]]
+        kl_timestamp = 1.0
 
         for x in range(0, alpha2):
             rating_fairness.append(kl_timestamp)
@@ -241,55 +165,12 @@ while iter < 500:
         if x > 1.0:
             x = 1.0
 
-        du += abs(G.node[node]["fairness"] - x)
-        G.node[node]["fairness"] = x
+        du += abs(G.nodes[node]["fairness"] - x)
+        G.nodes[node]["fairness"] = x
         #print mean_rating_fairness, kl_timestamp
     
     iter += 1
     if du < 0.01 and dp < 0.01 and dr < 0.01:
         break
 
-### SAVE THE RESULT
-currentfvals = []
-for node in nodes:
-    if "u" not in node[0]: # only store scores for edge generating nodes
-        continue
-    currentfvals.append(G.node[node]["fairness"])
-median_fvals = numpy.median(currentfvals)
-print (len(currentfvals), median_fvals)
-
-all_node_vals = []
-for node in nodes:
-    if "u" not in node[0]:
-        continue
-    f = G.node[node]["fairness"]
-    all_node_vals.append([node, (f - median_fvals)*numpy.log(G.out_degree(node)+1), f, G.out_degree(node)])
-all_node_vals = numpy.array(all_node_vals) #原本这里写的变量名为goodness_vals，修改为all_node_vals
-
-import operator
-# sort users based on their scores
-all_node_vals_sorted = sorted(all_node_vals, key= lambda x: (float(x[1]), float(x[2]), -1*float(x[3])))[::-1] 
-
-fw = open("./results/%s-fng-sorted-users-%d-%d-%d-%d-%d-%d-%d.csv" % (NETWORKNAME, alpha1, alpha2, beta1, beta2, gamma1, gamma2, gamma3),"w")
-
-#---------添加：根据data中groundtruth生成baduser和gooduser集--------#
-goodusers = set() #函数创建一个无序不重复元素集
-badusers = set()
-f = open("./data/%s/%s_gt.csv" % (NETWORKNAME, NETWORKNAME),"r")
-for l in f:
-        l = l.strip().split(",")
-        if l[1] == "-1":
-                badusers.add(l[0])
-        else:
-                goodusers.add(l[0])
-f.close()
-print(len(badusers), len(goodusers))
-#----------------------------------------------------------------#
-
-for i, sl in enumerate(all_node_vals_sorted):
-    if sl[3] in badusers or sl[3] in goodusers: # dont store users for which we dont have ground truth 没有groundtruth就注释掉
-        fw.write("%s,%s,%s,%s\n" %(str(sl[0]), str(sl[1]), str(sl[2]), str(sl[3])))
-fw.close()
-
-
-
+nx.write_gpickle(G, f"./results/{NETWORKNAME}_network.pkl")
